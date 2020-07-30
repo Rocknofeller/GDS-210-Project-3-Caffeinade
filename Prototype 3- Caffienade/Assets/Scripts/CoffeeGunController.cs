9cf4996fbdf5a78231a3e2fd20343902f1876b50
Jordan <1016971@student,sae,edu.au>
2020-07-30 22:29:41 +1000

    Shooting Coffee
    Basic Simple Shooting coffee, could be improved

---
+++ Prototype 3- Caffienade/Assets/Scripts/CoffeeGunController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeGunController : MonoBehaviour
+{
+	public Transform weaponHold;
+	public CoffeeGun startingGun;
+	CoffeeGun equippedGun;
+
+	void Start()
+	{
+		if (startingGun != null)
+		{
+			EquipGun(startingGun);
+		}
+	}
+
+	public void EquipGun(CoffeeGun gunToEquip)
+	{
+		if (equippedGun != null)
+		{
+			Destroy(equippedGun.gameObject);
+		}
+		equippedGun = Instantiate(gunToEquip, weaponHold.position, weaponHold.rotation) as CoffeeGun;
+		equippedGun.transform.parent = weaponHold;
+	}
+
+	//public void Shoot()
+	//{
+	//	if (equippedGun != null)
+	//	{
+	//		equippedGun.Shoot();
+	//	}
+	//}
+}
---
+++ Prototype 3- Caffienade/Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Wave[] waves;
+    public GameObject customer;
+
+    Wave currentWave;
+    int currentWaveNumber;
+
+    int customerRemainingToSpawn;
+    float nextSpawnTime;
+
+    void Update()
+    {
+        if (customerRemainingToSpawn > 0 && Time.time > nextSpawnTime)
+        {
+            customerRemainingToSpawn--;
+            nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
+
+            GameObject spawnedCustomer = Instantiate(customer, Vector3.zero, Quaternion.identity) as GameObject;
+        }
+    }
+
+    void NextWave()
+    {
+        currentWaveNumber ++;
+        currentWave = waves[currentWaveNumber - 1];
+
+        customerRemainingToSpawn = currentWave.CustomerCount;
+    }
+
+    [System.Serializable]
+    public class Wave
+    {
+        public int CustomerCount;
+        public float timeBetweenSpawns;
+    }
+}
